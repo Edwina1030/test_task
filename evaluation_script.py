1356c13828e3670099a2f34bae48752152851c2b
Tianheng_Ling <edwina1030@hotmail.com>
2020-11-11 13:35:26 +0100

    add codes to calculate analyze standard

--- evaluation_script.py
+++ evaluation_script.py
@@ -3,7 +3,7 @@ from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from dataloader import TorchDataset
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, accuracy_score, recall_score, f1_score
 import seaborn as sn
 import matplotlib.pyplot as plt
 
@@ -34,8 +34,9 @@ for img, label in test_loader:
 print('Model accuracy on test dataset:', test_acc / len(test_loader))
 print('Model loss on test dataset:', test_loss / len(test_loader))
 
-
-cm = confusion_matrix(label.reshape(-1), pred)
+y_true = label.reshape(-1)
+y_pred = pred
+cm = confusion_matrix(y_true, y_pred)
 
 xtick=['1','2','3','4','5','6','7','8','9','10']
 ytick=['1','2','3','4','5','6','7','8','9','10']
@@ -47,3 +48,28 @@ plt.ylabel('True label')
 plt.xlabel('Predicted label')
 plt.savefig("./confusion_matrix.jpg")
 plt.show()
+
+print("===="*6,'report',"===="*6)
+macro_precision = precision_score(y_true, y_pred,average='macro')
+print('macro precision = ', macro_precision)
+micro_precision = precision_score(y_true, y_pred,average='micro')
+print('micro precision = ', micro_precision)
+weighted_precision = precision_score(y_true, y_pred,average='weighted')
+print('weighted precision = ', weighted_precision)
+print("----"*15)
+accuracy = accuracy_score(y_true, y_pred)
+print('accuracy = ', accuracy)
+print("----"*15)
+macro_recall = recall_score(y_true, y_pred, average='macro')
+print('macro recall = ', macro_recall)
+micro_recall = recall_score(y_true, y_pred, average='micro')
+print('micro recall = ', micro_recall)
+weighted_recall = recall_score(y_true, y_pred, average='weighted')
+print('weighted recall = ', weighted_recall)
+print("----"*15)
+macro_f1_score = f1_score(y_true, y_pred, average='macro')
+print('macro F1 score = ', macro_f1_score)
+micro_f1_score = f1_score(y_true, y_pred, average='micro')
+print('micro F1 score = ', micro_f1_score)
+weighted_f1_score = f1_score(y_true, y_pred, average='weighted')
+print('weighted F1 score = ', weighted_f1_score)
